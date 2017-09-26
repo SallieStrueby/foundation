a4f04a9ac95ea3c8c66e905bcbf4e8a1d15572fc
SallieStrueby <32113671+SallieStrueby@users.noreply.github.com>
2017-09-26 16:54:00 -0600

    Update Data Viz Assignment One.R

--- Data Viz Assignment One.R
+++ Data Viz Assignment One.R
@@ -1,3 +1,34 @@
-Age
-Name
-MakeIntroduction Function
+# Part One Introductions
+my.age <- 28
+my.name <- "Sallie"
+MakeIntroduction <- function(my.name, my.age)
+MakeIntroduction <- paste("Hello, my name is", my.name, "and I am", my.age, "years old")
+MakeIntroduction
+# variable for my.intro
+my.intro <- MakeIntroduction(my.name, my.age)
+my.intro
+# variable for casual intro
+casual.intro <- sub("Hello, my name is","Hey, I'm", my.intro)
+print(casual.intro)
+library(stringr)
+capitol.intro <- str_to_upper(my.intro)
+intro.e.count <- str_count(my.intro, pattern = 'e')
+# Part Two Books
+Mybooks <- c("Salem's Lot", "Harry Potter", "Lord of the Rings", "Hunger Games", "Wizard of Earth Sea ", "Neverwhere")
+print(Mybooks)
+top.three.books <- Mybooks[1:3]
+book.reviews <-"is a great read!"
+paste(Mybooks, book.reviews)
+#variable books without four
+books.without.four <- (Mybooks[-4])
+books.without.four
+long.titles <- c("Salem's Lot", "Harry Potter", "Lord of the Rings", "Hunger Games", "Wizard of Earth Sea ", "Neverwhere")
+title.is.long <- str_count(long.titles) >15
+# Part Three Squares
+Numbers <- c(1:201)
+squared_numbers <- Numbers * Numbers
+squared_mean <- mean(squared_numbers)
+Squares <- sqrt(Numbers)
+Squares
+answer <- round(Squares)
+answer
